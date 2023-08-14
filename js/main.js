1bc29d45e9ceb974cf1f23bc20008f2de4e50d17
Suryawandivta <gitdivta@gmail.com>
2023-08-14 10:18:50 +0800

    update js home

--- js/main.js
+++ js/main.js
@@ -1,18 +1,20 @@
 // text in home
 const typingText = document.getElementById('typing-text');
-const texts = ["Junior Web Developer 😃"];
+const staticText = "Junior ";
+const texts = ["Web Developer 😉"];
 let textIndex = 0;
 let charIndex = 0;
 let isErasing = false;
 
 function typeText() {
     const text = texts[textIndex];
+    const fullText = staticText + text;
 
     if (isErasing) {
-        typingText.textContent = text.substr(0, charIndex);
+        typingText.textContent = fullText.substr(0, charIndex);
         charIndex--;
 
-        if (charIndex >= 0) {
+        if (charIndex >= staticText.length) {
             setTimeout(typeText, 100); // Delay between erasing each character
         } else {
             isErasing = false;
@@ -20,10 +22,10 @@ function typeText() {
             setTimeout(typeText, 500); // Delay before typing the next text
         }
     } else {
-        typingText.textContent = text.substr(0, charIndex);
+        typingText.textContent = fullText.substr(0, charIndex);
         charIndex++;
 
-        if (charIndex <= text.length) {
+        if (charIndex <= fullText.length) {
             setTimeout(typeText, 100); // Delay between typing each character
         } else {
             isErasing = true;
@@ -32,6 +34,3 @@ function typeText() {
     }
 }
 typeText();
-
-
-
